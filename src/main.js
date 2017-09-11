6d07dd4136364174a565344417c2fbb8ceb8574d
Amar <amarbham@hotmail.com>
2017-09-11 12:36:53 +0100

    Fix closure

--- src/main.js
+++ src/main.js
@@ -1,16 +1,10 @@
 const buttons = Array.from(document.getElementsByTagName('button'));
 
-function createCount(name) {
-  let count = 0;
-  return () => {
-    count += 1;
-    return `${name} clicked ${count} times`;
-  };
-}
-
 function createHandler(name) {
-  return () => {
-    createCount(name);
+  let count = 0;
+  return () => {  /* The closure */
+    count += 1;
+    console.log(`${name} clicked ${count} times`);
   };
 }
 
